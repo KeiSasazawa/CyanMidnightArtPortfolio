b6f78a28fdc6c388d4dd831b1325a9a5748d0522
KeiSasazawa <raisasasazawa@gmail.com>
2025-04-22 19:39:03 +0900

    Translate all pages to japanese
    All pages recieved its japanese tranlation. This
    will result to all pages to switch languages when
    clicking the language toggle button.

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const langs = document.querySelector('.langWrap');
     const link = document.querySelectorAll('.langWrap a');
-
     const home = document.querySelector('.home');
     const gallery = document.querySelector('.gallery');
     const commission = document.querySelector('.commission');
-
     const heroMainText = document.querySelector('.heroMainText');
     const featuredTitle = document.querySelector('.featuredTitle');
     const aboutTitle = document.querySelector('.aboutTitle');
@@ -14,12 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const subText = document.querySelector('.subText');
     const acceptCommission = document.querySelector('.acceptCommission');
     const acceptDescription = document.querySelector('.acceptDescription');
+    const galleryHead = document.querySelector('.galleryHead');
 
     const data = {
         english: {
             home: "Home",
             gallery: "Gallery",
             commission: "Commission",
+            
             heroMainText: "Welcome to Cyan Midnight Art",
             featuredTitle: "Featured Art",
             acceptCommission: "I accept commissions!",
@@ -27,7 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
             aboutTitle: "About Me",
             quote1: "Hi I'm Kei, the artist behind Cyan Midnight. I started my art journey to express emotions, dreams, and the things I love into my pieces.",
             quote2: "I work with digital mediums and I love playing with colors to create art with pastel feel.",
-            subText: "Where imagination meets dreamy hues! I'm Kei - a digital artist sharing surreal, dynamic, and whimsical art."
+            subText: "Where imagination meets dreamy hues! I'm Kei - a digital artist sharing surreal, dynamic, and whimsical art.",
+            galleryHead: "Gallery"
         },
         japanese: {
             home: "ホーム",
@@ -40,7 +41,8 @@ document.addEventListener('DOMContentLoaded', () => {
             aboutTitle: "私について",
             quote1: "こんにちは、Cyan Midnightのアーティスト、Keiです。感情や夢、好きなものを作品に込めたくて、アートを始めました。",
             quote2: "デジタルアートで、パステルな雰囲気の色使いを楽しみながら描いています。",
-            subText: "想像した夢をパステルカラーの絵に"
+            subText: "想像した夢をパステルカラーの絵に",
+            galleryHead: "ギャラリー",
             
         }
     };
@@ -65,6 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
             subText.textContent = data[attr].subText;
             acceptCommission.textContent = data[attr].acceptCommission;
             acceptDescription.textContent = data[attr].acceptDescription;
+            galleryHead.textContent = data[attr].galleryHead;
         });
     });
 });
